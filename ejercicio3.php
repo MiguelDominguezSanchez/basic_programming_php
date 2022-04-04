a87db3b08c242a7baf7e3db4c24d93b4313600e2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-04 17:07:17 +0200

    If Conditional

---
+++ ejercicio3.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ Escribe un programa que imprima por pantalla los
+cuadrados (el número multiplicado por sí mismo) de
+los 30 primeros númerros naturales.
+
+Modifica el ejercicio anterior para que muestre al
+lado de cada cuadrado si es un número par o impar.
+*/
+
+for($i = 1; $i <= 30; $i++) {
+
+    $cuadrado = $i*$i;
+
+    echo "El cuadrado de ".$i." es ".$cuadrado;
+
+    if($cuadrado%2 == 0){
+        echo " y es par";
+    } else {
+        echo " y ES IMPAR";
+    }
+
+    echo "<br/>";
+}
+?>
